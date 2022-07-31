934515f2cc606f0ef2fbf89092da65954dcf50cd
softwaredevelop <61334390+softwaredevelop@users.noreply.github.com>
2022-07-31 17:41:42 +0000

    refactor: Add bash prompt config

--- .devcontainer/library-scripts/setup-user-alpine.sh
+++ .devcontainer/library-scripts/setup-user-alpine.sh
@@ -70,9 +70,75 @@ if [ "${USERNAME}" != "root" ] && [ "${EXISTING_NON_ROOT_USER}" != "${USERNAME}"
   EXISTING_NON_ROOT_USER="${USERNAME}"
 fi
 
+rc_snippet="$(
+  cat <<'EOF'
+if [ -z "${USER}" ]; then
+  USER=$(whoami)
+  export USER
+fi
+if [[ "${PATH}" != *"$HOME/.local/bin"* ]]; then export PATH="${PATH}:$HOME/.local/bin"; fi
+if [ -z "$(git config --get core.editor)" ] && [ -z "${GIT_EDITOR}" ]; then
+  if [ "${TERM_PROGRAM}" = "vscode" ]; then
+    if [[ -n $(command -v code-insiders) && -z $(command -v code) ]]; then
+      export GIT_EDITOR="code-insiders --wait"
+    else
+      export GIT_EDITOR="code --wait"
+    fi
+  fi
+fi
+EOF
+)"
+
+codespaces_bash="$(
+  cat \
+    <<'EOF'
+# Codespaces bash prompt theme
+__bash_prompt() {
+    local userpart='`export XIT=$? \
+        && [ ! -z "${GITHUB_USER}" ] && echo -n "\[\033[0;32m\]@${GITHUB_USER} " || echo -n "\[\033[0;32m\]\u " \
+        && [ "$XIT" -ne "0" ] && echo -n "\[\033[1;31m\]➜" || echo -n "\[\033[0m\]➜"`'
+    local gitbranch='`\
+        if [ "$(git config --get codespaces-theme.hide-status 2>/dev/null)" != 1 ]; then \
+            export BRANCH=$(git symbolic-ref --short HEAD 2>/dev/null || git rev-parse --short HEAD 2>/dev/null); \
+            if [ "${BRANCH}" != "" ]; then \
+                echo -n "\[\033[0;36m\](\[\033[1;31m\]${BRANCH}" \
+                && if git ls-files --error-unmatch -m --directory --no-empty-directory -o --exclude-standard ":/*" > /dev/null 2>&1; then \
+                        echo -n " \[\033[1;33m\]✗"; \
+                fi \
+                && echo -n "\[\033[0;36m\]) "; \
+            fi; \
+        fi`'
+    local lightblue='\[\033[1;34m\]'
+    local removecolor='\[\033[0m\]'
+    PS1="${userpart} ${lightblue}\w ${gitbranch}${removecolor}\$ "
+    unset -f __bash_prompt
+}
+__bash_prompt
+EOF
+)"
+
+if [ "${USERNAME}" = "root" ]; then
+  user_rc_path="/root"
+else
+  user_rc_path="/home/${USERNAME}"
+fi
+
+if [ "${RC_SNIPPET_ALREADY_ADDED}" != "true" ]; then
+  echo "${rc_snippet}" >>/etc/bash.bashrc
+  echo "${codespaces_bash}" >>"${user_rc_path}/.bashrc"
+  echo 'export PROMPT_DIRTRIM=4' >>"${user_rc_path}/.bashrc"
+  if [ "${USERNAME}" != "root" ]; then
+    echo "${codespaces_bash}" >>"/root/.bashrc"
+    echo 'export PROMPT_DIRTRIM=4' >>"/root/.bashrc"
+  fi
+  chown ${USERNAME}:${group_name} "${user_rc_path}/.bashrc"
+  RC_SNIPPET_ALREADY_ADDED="true"
+fi
+
 if [ ! -d "$MARKER_FILE_DIR" ]; then
   mkdir -p "$MARKER_FILE_DIR"
 fi
 
 echo -e "\
-    EXISTING_NON_ROOT_USER=${EXISTING_NON_ROOT_USER}" >"${MARKER_FILE}"
+    EXISTING_NON_ROOT_USER=${EXISTING_NON_ROOT_USER}\n\
+    RC_SNIPPET_ALREADY_ADDED=${RC_SNIPPET_ALREADY_ADDED}" >"${MARKER_FILE}"
