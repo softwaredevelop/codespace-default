f35eabaaaaf794d9f3d3e4072e7b26fb8dc8fea3
softwaredevelop <61334390+softwaredevelop@users.noreply.github.com>
2022-08-03 15:08:22 +0000

    test: Add non_root_user

--- test/test.sh
+++ test/test.sh
@@ -4,8 +4,9 @@ SCRIPT_FOLDER_NAME=$(dirname $0)
 cd ${SCRIPT_FOLDER_NAME} || exit
 
 # shellcheck source=/dev/null
-source test-utils.sh
+source test-utils.sh codespace
 
+check "non-root-user" non_root_user
 check "hadolint" hadolint --version
 check "shfmt" shfmt --version
 check "shellcheck" shellcheck --version
