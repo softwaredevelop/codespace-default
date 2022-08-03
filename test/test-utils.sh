d7dce8b3cef547222334deab5484d5734964ec5e
softwaredevelop <61334390+softwaredevelop@users.noreply.github.com>
2022-08-03 13:01:20 +0000

    test: Add test-utils.sh

---
+++ test/test-utils.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+FAILED_COUNT=0
+
+if [ -z $HOME ]; then
+  HOME="/root"
+fi
+
+function echoStderr() {
+  echo "$@" 1>&2
+}
+
+function check() {
+  LABEL=$1
+  shift
+  echo -e "\n🧪 Testing: ${LABEL}"
+  if "$@"; then
+    echo "✅ Passed: ${LABEL}"
+    return 0
+  else
+    echoStderr "❌ Failed: ${LABEL}"
+    FAILED_COUNT=$((FAILED_COUNT + 1))
+    return 1
+  fi
+}
+
+function reportResults() {
+  if [ ${FAILED_COUNT} -eq 0 ]; then
+    hundred="💯"
+    printf "\n%s All tests passed!\n" "${hundred}"
+    exit 0
+  else
+    boom="💥"
+    printf "\n%s Failed tests: %s\n" "${boom}" "${FAILED_COUNT}"
+    exit 1
+  fi
+}
