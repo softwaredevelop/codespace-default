cdf0ddaf0aa10f989a9d5ea2a4392a85acdcc32c
softwaredevelop <61334390+softwaredevelop@users.noreply.github.com>
2022-08-04 21:22:24 +0000

    test: Add checkOSPackages

--- test/test.sh
+++ test/test.sh
@@ -6,6 +6,29 @@ cd ${SCRIPT_FOLDER_NAME} || exit
 # shellcheck source=/dev/null
 source test-utils.sh codespace
 
+if [ "${ID_OS}" = "ubuntu" ]; then
+  package_list_debian="apt-utils \
+        apt-transport-https \
+        ca-certificates \
+        curl \
+        git \
+        gnupg \
+        locales \
+        lsb-release \
+        sudo \
+        wget \
+        xz-utils"
+  checkOSPackages "common-os-packages" ${package_list_debian}
+elif [ "${ID_OS}" = "alpine" ]; then
+  package_list_alpine="ca-certificates \
+        curl \
+        dpkg \
+        git \
+        libc6-compat \
+        shadow \
+        sudo"
+  checkOSPackages "common-os-packages" ${package_list_alpine}
+fi
 check "non-root-user" non_root_user
 check "hadolint" hadolint --version
 check "shfmt" shfmt --version
