7b3de2b91c0a39fe8905c62cbb517da80785b721
softwaredevelop <61334390+softwaredevelop@users.noreply.github.com>
2024-03-13 14:11:14 +0100

    test: Add actionlint test for actionlint run

--- cc/linting/al_test.go
+++ cc/linting/al_test.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"testing"
 
+	"dagger.io/dagger"
 	"github.com/stretchr/testify/require"
 )
 
@@ -12,6 +13,22 @@ func TestActionlint(t *testing.T) {
 	t.Parallel()
 	ctx := context.Background()
 
+	t.Run("Test_actionlint_run", func(t *testing.T) {
+		t.Parallel()
+		c = c.Pipeline("test_actionlint_run")
+		require.NotNil(t, c)
+
+		_, err := linting.Al(c, id).
+			WithNewFile("test.yml", dagger.ContainerWithNewFileOpts{
+				Contents: `{name: "Test workflow", on: {push: {branches: ["main"]}}, jobs: {build: {runs-on: "ubuntu-latest", steps: [{name: "Checkout code", uses: "actions/checkout@v2"}, {name: "Run a one-line script", run: "echo Hello, world!"}]}}}`,
+			}).
+			WithNewFile("test2.yml", dagger.ContainerWithNewFileOpts{
+				Contents: `{name: "Test2 workflow", on: {push: {branches: ["main"]}}}`,
+			}).
+			WithExec([]string{"sh", "-c", "/actionlint $(find . -type f -name '*.yml')"}).
+			Stdout(ctx)
+		require.ErrorContains(t, err, "is missing")
+	})
 	t.Run("Test_actionlint_version", func(t *testing.T) {
 		t.Parallel()
 		c = c.Pipeline("test_actionlint_version")
