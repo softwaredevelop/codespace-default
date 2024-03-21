177b8bce5c11924a1de4c9fd421ce3127468d49a
softwaredevelop <61334390+softwaredevelop@users.noreply.github.com>
2024-03-22 00:16:37 +0100

    test: Refactor linting tests and add Trivy integration test

--- cc/linting/t_test.go
+++ cc/linting/t_test.go
@@ -15,7 +15,23 @@ func TestT(t *testing.T) {
 
 	t.Run("Test_trivy_run", func(t *testing.T) {
 		t.Parallel()
-		c = c.Pipeline("test_actionlint_run")
+		c = c.Pipeline("test_trivy_run")
+		require.NotNil(t, c)
+
+		_, err := linting.T(c, id).
+			WithNewFile(".trivyignore", dagger.ContainerWithNewFileOpts{
+				Contents: "AVD-DS-0002",
+			}).
+			WithNewFile("Dockerfile.test", dagger.ContainerWithNewFileOpts{
+				Contents: "FROM docker.io/library/alpine:$VARIANT\nHEALTHCHECK none",
+			}).
+			WithExec([]string{"sh", "-c", "/trivy config --severity=MEDIUM,HIGH,CRITICAL --exit-code=1 $(find . -type f \\( -name Dockerfile -o -name Dockerfile.* \\))"}).
+			Stdout(ctx)
+		require.NoError(t, err)
+	})
+	t.Run("Test_trivy_run_error", func(t *testing.T) {
+		t.Parallel()
+		c = c.Pipeline("test_trivy_run_error")
 		require.NotNil(t, c)
 
 		_, err := linting.T(c, id).
